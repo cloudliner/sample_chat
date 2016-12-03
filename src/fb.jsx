176b2cd8bdb5ab4c5a9460951716378b9c13e8bd
cloudliner <cloudliner.jp@gmail.com>
2016-12-03 16:58:05 +0900

    Step 3

--- src/fb.jsx
+++ src/fb.jsx
@@ -49,30 +49,73 @@ export function handleAddedMessage(callback) {
   });
 }
 
-///////step2で追加/////////
+///////step2のコードをstep3で書き換え/////////
 var provider = new firebase.auth.TwitterAuthProvider();
+var ref = null;
+
 export function login() {
-  firebase.auth().signInWithRedirect(provider);
+  //Webかスマホアプリかで処理を分岐
+  if(typeof cordova==="undefined"){
+    firebase.auth().signInWithRedirect(provider);
+  }else{
+    ref = window.open('https://{ここをプロジェクト名に置き換え}.firebaseapp.com/login.html?t=' + new Date().getTime(), '_blank', 'location=yes');
+    ref.addEventListener('loadstart', handleLoadStart);
+  }
 }
 
-firebase.auth().getRedirectResult().then(function(result) {
-  console.log("login");
-  if(result.user) {
-    user = {
-      uid:result.user.uid,
-      displayName:result.user.displayName,
-      photoURL:result.user.photoURL,
-      signin:true
+function handleLoadStart(event) {
+    var params = getUrlVars(event.url);
+    if(event.url.indexOf("close.html") > -1) {
+      if(params["uid"]) {
+        ref.removeEventListener('loadstart', handleLoadStart);
+        ref.close();
+        ref = null;
+        user = {
+          uid:params["uid"],
+          displayName:decodeURI(params["displayName"]),
+          photoURL:"https://" + params["photoURL"].substring("http://".length),
+          signin:true
+        }
+        applyChangedUserCallbacks.forEach(function(callback) {
+          callback(getUser());
+        });
+      }
     }
-    applyChangedUserCallbacks.forEach(function(callback) {
-      callback(getUser());
-    });
+}
+function getUrlVars(url) {
+  var vars = [], max = 0, hash = "", array = "";
+  hash  = url.split('?')[1].split('&');
+  max = hash.length;
+  for (var i = 0; i < max; i++) {
+    array = hash[i].split('=');
+    vars.push(array[0]);
+    vars[array[0]] = array[1];
   }
-}).catch(function(error) {
-  console.log(error);
-  alert("ログインに失敗しました:" + error.message);
-});
+  return vars;
+}
 
+
+
+if(typeof cordova==="undefined"){
+  //この処理はWeb用なのでスマホアプリ上では実行しない
+  firebase.auth().getRedirectResult().then(function(result) {
+    console.log("login");
+    if(result.user) {
+      user = {
+        uid:result.user.uid,
+        displayName:result.user.displayName,
+        photoURL:"https://" + result.user.photoURL.substring("http://".length),
+        signin:true
+      }
+      applyChangedUserCallbacks.forEach(function(callback) {
+        callback(getUser());
+      });
+    }
+  }).catch(function(error) {
+    console.log(error);
+    alert("ログインに失敗しました:" + error.message);
+  });
+}
 
 export function logout() {
   console.log("logout");
